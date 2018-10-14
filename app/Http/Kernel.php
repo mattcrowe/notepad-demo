f3c4a42c983bbf8c383f0ee48a2b6bcec374a5cd
Matt Crowe <github.mpc@zym.me>
2018-10-14 18:35:13 +0000

    continue notes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,11 +30,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-
-        'admin' => [
-            'web',
-        ],
-
         'web' => [
             Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             Illuminate\Session\Middleware\StartSession::class,
@@ -43,7 +38,6 @@ class Kernel extends HttpKernel
             App\Http\Middleware\VerifyCsrfToken::class,
             Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
         'api' => [
             App\Http\Middleware\EncryptCookies::class,
             Illuminate\Session\Middleware\StartSession::class,
